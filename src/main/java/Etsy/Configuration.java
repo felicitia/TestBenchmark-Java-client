a05702ae0a41fecfc3fc9ef71f6d6f78159f0532
Justin Chen <justinbrianchen@yahoo.com>
2019-08-02 21:24:03 -0700

    wrote representative test cases for Etsy

--- src/main/java/Etsy/Configuration.java
+++
@@ -1,7 +0,0 @@
-package Etsy;
-
-public class Configuration {
-
-	static final String email_or_username = "aaa";
-	static final String password = "bbb";
-}
--- src/main/java/Wish/Configuration.java
+++
@@ -1,7 +0,0 @@
-package Wish;
-
-public class Configuration {
-
-	static final String email = "aaa";
-	static final String password = "bbb";
-}
