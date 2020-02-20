446c5734ce6773a3fad433091faebd2bcdf2c14d
MarceloLaser <marcelo.laser@gmail.com>
2020-02-19 19:49:23 -0800

    added signin and signup test cases to theguardian

--- src/main/java/news/theguardian/RepresentativeTests.java
+++ src/main/java/news/theguardian/RepresentativeTests.java
@@ -43,12 +43,76 @@ public class RepresentativeTests {
 
   @Test
   public void testSignIn() {
-
+    //class android.widget.ImageButton
+    //activity .feature.stream.NewHomeActivity
+    AndroidElement menu = (AndroidElement) driver.findElementById("com.guardian:id/fabHome");
+    menu.click();
+    //class android.widget.ImageView
+    //activity .feature.stream.NewHomeActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.guardian:id/nav_drawer_settings_button");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .feature.setting.SettingsActivity
+    AndroidElement settings_tosignin = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[2]");
+    settings_tosignin.click();
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signin_email = (AndroidElement) driver.findElementById("com.guardian:id/email");
+    signin_email.sendKeys(Configuration.email);
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signin_password = (AndroidElement) driver.findElementById("com.guardian:id/password");
+    signin_password.sendKeys(Configuration.password);
+    //class android.widget.LinearLayout
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement sign_in = (AndroidElement) driver.findElementById("com.guardian:id/sign_in_button");
+    sign_in.click();
+    //activity .feature.setting.SettingsActivity
   }
 
   @Test
   public void testSignUp() {
-
+    //class android.widget.ImageButton
+    //activity .feature.stream.NewHomeActivity
+    AndroidElement menu = (AndroidElement) driver.findElementById("com.guardian:id/fabHome");
+    menu.click();
+    //class android.widget.ImageView
+    //activity .feature.stream.NewHomeActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.guardian:id/nav_drawer_settings_button");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .feature.setting.SettingsActivity
+    AndroidElement settings_tosignin = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[2]");
+    settings_tosignin.click();
+    //class android.widget.FrameLayout
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signin_tosignup = (AndroidElement) driver.findElementById("com.guardian:id/bottom_bar");
+    signin_tosignup.click();
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signup_email = (AndroidElement) driver.findElementById("com.guardian:id/register_email");
+    signup_email.sendKeys(Configuration.email);
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signup_password = (AndroidElement) driver.findElementById("com.guardian:id/register_password");
+    signup_password.sendKeys(Configuration.password);
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signup_firstname = (AndroidElement) driver.findElementById("com.guardian:id/register_first_name");
+    signup_firstname.sendKeys(Configuration.firstName);
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signup_lastname = (AndroidElement) driver.findElementById("com.guardian:id/register_last_name");
+    signup_lastname.sendKeys(Configuration.lastName);
+    //class android.widget.EditText
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement signup_username = (AndroidElement) driver.findElementById("com.guardian:id/register_username");
+    signup_username.sendKeys(Configuration.username);
+    //class android.widget.LinearLayout
+    //activity .feature.login.ui.LoginActivity
+    AndroidElement sign_up = (AndroidElement) driver.findElementById("com.guardian:id/register_button");
+    sign_up.click();
+    //activity .feature.setting.SettingsActivity
   }
 
   @Test
