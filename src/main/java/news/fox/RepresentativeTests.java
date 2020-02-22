532b213562bed3271fcad661de6f20f2e563c4db
MarceloLaser <marcelo.laser@gmail.com>
2020-02-21 18:31:09 -0800

    added fox

--- src/main/java/news/fox/RepresentativeTests.java
+++ src/main/java/news/fox/RepresentativeTests.java
@@ -41,73 +41,173 @@ public class RepresentativeTests {
       driver.resetApp();
   }
 
-  @Test
-  public void testSignIn() {
+  // @Test
+  // public void testSignIn() {
+  //   //does not have this functionality
+  // }
 
-  }
+  // @Test
+  // public void testSignUp() {
+  //   //does not have this functionality
+  // }
 
-  @Test
-  public void testSignUp() {
-
-  }
-
-  @Test
-  public void testSearch() {
-
-  }
+  // @Test
+  // public void testSearch() {
+  //   //does not have this functionality
+  // }
 
   @Test
   public void testDetail() {
-
+    //class android.view.ViewGroup
+    //activity .corenav.StartActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.foxnews.android:id/big_top_item_container");
+    item_1.click();
+    //activity .feature.articledetail.ArticleDetailActivity
   }
 
   @Test
   public void testAddBookmark() {
-
+    //class android.view.ViewGroup
+    //activity .corenav.StartActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.foxnews.android:id/big_top_item_container");
+    item_1.click();
+    //class android.widget.ImageView
+    //activity .feature.articledetail.ArticleDetailActivity
+    AndroidElement save_bookmark = (AndroidElement) driver.findElementById("com.foxnews.android:id/save");
+    save_bookmark.click();
+    //activity .feature.articledetail.ArticleDetailActivity
   }
 
   @Test
   public void testRemoveBookmark() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //class android.view.ViewGroup
+    //activity .corenav.StartActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.foxnews.android:id/component_headline_item_container");
+    item_1.click();
+    //class android.widget.ImageView
+    //activity .feature.articledetail.ArticleDetailActivity
+    AndroidElement remove_bookmark = (AndroidElement) driver.findElementById("com.foxnews.android:id/save");
+    remove_bookmark.click();
+    //activity .feature.articledetail.ArticleDetailActivity
   }
 
   @Test
   public void testAccount() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //activity .corenav.StartActivity
   }
 
   @Test
   public void testTextSize() {
-
+    //class android.view.ViewGroup
+    //activity .corenav.StartActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.foxnews.android:id/big_top_item_container");
+    item_1.click();
+    //class android.widget.ImageView
+    //activity .feature.articledetail.ArticleDetailActivity
+    AndroidElement change_textsize = (AndroidElement) driver.findElementById("com.foxnews.android:id/scale_text");
+    change_textsize.click();
+    //activity .feature.articledetail.ArticleDetailActivity
   }
 
   @Test
   public void testAbout() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //class android.widget.TextView
+    //activity .corenav.StartActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.foxnews.android:id/settings_bttn");
+    menu_settings.click();
+    //class android.widget.FrameLayout
+    //activity .foryou.SettingsActivity
+    AndroidElement about = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[2]");
+    about.click();
+    //activity .common.web.WebViewActivity
   }
 
   @Test
   public void testCategory() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement category = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_browse");
+    category.click();
+    //class android.widget.TextView
+    //activity .corenav.StartActivity
+    AndroidElement cat_1 = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.TextView[1]");
+    cat_1.click();
+    //activity .feature.topic.TopicActivity
   }
 
   @Test
   public void testContact() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //class android.widget.TextView
+    //activity .corenav.StartActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.foxnews.android:id/settings_bttn");
+    menu_settings.click();
+    //class android.widget.FrameLayout
+    //activity .foryou.SettingsActivity
+    AndroidElement contact = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[10]");
+    contact.click();
+    //activity .common.web.WebViewActivity
   }
 
   @Test
   public void testHelp() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //class android.widget.TextView
+    //activity .corenav.StartActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.foxnews.android:id/settings_bttn");
+    menu_settings.click();
+    //class android.widget.FrameLayout
+    //activity .foryou.SettingsActivity
+    AndroidElement help = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[3]");
+    help.click();
+    //activity .common.web.WebViewActivity
   }
 
   @Test
   public void testMenu() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //class android.widget.TextView
+    //activity .corenav.StartActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.foxnews.android:id/settings_bttn");
+    menu_settings.click();
+    //activity .foryou.SettingsActivity
   }
 
   @Test
   public void testTerms() {
-
+    //class androidx.appcompat.widget.LinearLayoutCompat
+    //activity .corenav.StartActivity
+    AndroidElement menu_account = (AndroidElement) driver.findElementById("com.foxnews.android:id/navbar_for_you");
+    menu_account.click();
+    //class android.widget.TextView
+    //activity .corenav.StartActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("com.foxnews.android:id/settings_bttn");
+    menu_settings.click();
+    //class android.widget.FrameLayout
+    //activity .foryou.SettingsActivity
+    AndroidElement legal = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[4]");
+    legal.click();
+    //activity .common.web.WebViewActivity
   }
 }
