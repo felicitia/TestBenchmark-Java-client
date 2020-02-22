3b88a230dd70685db0c1330ea72947b6072e1b7f
MarceloLaser <marcelo.laser@gmail.com>
2020-02-21 17:22:57 -0800

    adding smartnews

--- src/main/java/news/smartnews/RepresentativeTests.java
+++ src/main/java/news/smartnews/RepresentativeTests.java
@@ -41,73 +41,153 @@ public class RepresentativeTests {
       driver.resetApp();
   }
 
-  @Test
-  public void testSignIn() {
+  // @Test
+  // public void testSignIn() {
+  //   //does not have this functionality
+  // }
 
-  }
-
-  @Test
-  public void testSignUp() {
-
-  }
+  // @Test
+  // public void testSignUp() {
+  //   //does not have this functionality
+  // }
 
   @Test
   public void testSearch() {
-
+    //class android.widget.TextView
+    //activity .activity.MainActivity
+    AndroidElement tosearch = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/searchTextView");
+    tosearch.click();
+    //class android.widget.EditText
+    //activity .search.SearchActivity
+    AndroidElement search_bar = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/search_src_text");
+    search_bar.sendKeys("llama\n");
+    //activity .search.SearchActivity
   }
 
   @Test
   public void testDetail() {
-
+    //class android.widget.TextView
+    //activity .activity.MainActivity
+    AndroidElement tosearch = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/searchTextView");
+    tosearch.click();
+    //class android.widget.EditText
+    //activity .search.SearchActivity
+    AndroidElement search_bar = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/search_src_text");
+    search_bar.sendKeys("llama\n");
+    //class android.widget.LinearLayout
+    //activity .search.SearchActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]");
+    item_1.click();
+    //activity .search.SearchActivity
   }
 
-  @Test
-  public void testAddBookmark() {
+  // @Test
+  // public void testAddBookmark() {
+  //   //does not have this functionality
+  // }
 
-  }
+  // @Test
+  // public void testRemoveBookmark() {
+  //   //does not have this functionality
+  // }
 
-  @Test
-  public void testRemoveBookmark() {
-
-  }
-
-  @Test
-  public void testAccount() {
-
-  }
+  // @Test
+  // public void testAccount() {
+  //   //does not have this functionality
+  // }
 
   @Test
   public void testTextSize() {
-
+    //class android.widget.ImageButton
+    //activity .activity.MainActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/settingButton");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .activity.SettingActivity
+    AndroidElement menu_textsize = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[3]");
+    menu_textsize.click();
+    //class android.widget.CheckedTextView
+    //activity .activity.SettingActivity
+    AndroidElement change_textsize = (AndroidElement) driver.findElementById("android:id/text1");
+    change_textsize.click();
+    //activity .activity.SettingActivity
   }
 
   @Test
   public void testAbout() {
-
+    //class android.widget.ImageButton
+    //activity .activity.MainActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/settingButton");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .activity.SettingActivity
+    AndroidElement about = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[10]");
+    about.click();
+    //activity .activity.SettingAboutActivity
   }
 
   @Test
   public void testCategory() {
-
+    //class android.view.View
+    //activity .activity.MainActivity
+    AndroidElement cat_1 = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.RelativeLayout/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.view.View[1]");
+    cat_1.click();
+    //activity .activity.MainActivity
   }
 
   @Test
   public void testContact() {
-
+    //class android.widget.ImageButton
+    //activity .activity.MainActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/settingButton");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .activity.SettingActivity
+    AndroidElement help = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[7]");
+    help.click();
+    //class android.view.View
+    //activity .support.SupportActivity
+    AndroidElement contact = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View[1]/android.view.View[2]/android.view.View[2]/android.view.View");
+    contact.click();
+    //activity .support.SupportActivity
   }
 
   @Test
   public void testHelp() {
-
+    //class android.widget.ImageButton
+    //activity .activity.MainActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/settingButton");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .activity.SettingActivity
+    AndroidElement help = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[7]");
+    help.click();
+    //activity .support.SupportActivity
   }
 
   @Test
   public void testMenu() {
-
+    //class android.widget.ImageButton
+    //activity .activity.MainActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/settingButton");
+    menu_settings.click();
+    //activity .activity.SettingActivity
   }
 
   @Test
   public void testTerms() {
-
+    //class android.widget.ImageButton
+    //activity .activity.MainActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementById("jp.gocro.smartnews.android:id/settingButton");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .activity.SettingActivity
+    AndroidElement about = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.ListView/android.widget.LinearLayout[10]");
+    about.click();
+    //class android.widget.LinearLayout
+    //activity .activity.SettingAboutActivity
+    AndroidElement legal = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ListView/android.widget.LinearLayout[2]");
+    legal.click();
+    //activity .activity.WebPageActivity
   }
 }
