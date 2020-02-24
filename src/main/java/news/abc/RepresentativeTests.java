71941d3dea58b169e4987eb039ed466e63fdf653
MarceloLaser <marcelo.laser@gmail.com>
2020-02-24 15:16:02 -0800

    added abc

--- src/main/java/news/abc/RepresentativeTests.java
+++ src/main/java/news/abc/RepresentativeTests.java
@@ -41,73 +41,169 @@ public class RepresentativeTests {
       driver.resetApp();
   }
 
-  @Test
-  public void testSignIn() {
+  // @Test
+  // public void testSignIn() {
+  //   //pop not detected by appium, abort
+  // }
 
-  }
+  // @Test
+  // public void testSignUp() {
+  //   //class androidx.appcompat.app.ActionBar$Tab
+  //   //activity .ui.navigation.MainNavigationActivity
+  //   AndroidElement category = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.HorizontalScrollView/android.widget.LinearLayout/androidx.appcompat.app.ActionBar.Tab[2]");
+  //   category.click();
+  //   //class android.widget.RelativeLayout
+  //   //activity .ui.navigation.MainNavigationActivity
+  //   AndroidElement home_signin_or_signup = (AndroidElement) driver.findElementById("com.abc.abcnews:id/login_banner");
+  //   home_signin_or_signup.click();
+  //   //pop not detected by appium, abort
+  // }
 
-  @Test
-  public void testSignUp() {
-
-  }
-
-  @Test
-  public void testSearch() {
-
-  }
+  // @Test
+  // public void testSearch() {
+  //   //class android.widget.ImageButton
+  //   //activity .ui.navigation.MainNavigationActivity
+  //   AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+  //   menu.click();
+  //   //class android.widget.EditText
+  //   //activity .ui.navigation.MainNavigationActivity
+  //   AndroidElement search_bar = (AndroidElement) driver.findElementById("com.abc.abcnews:id/drawer_search");
+  //   search_bar.sendKeys("llama\n");
+  //   //couldn't finish because \n doesn't work
+  // }
 
   @Test
   public void testDetail() {
-
+    //class android.view.View
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.abc.abcnews:id/cinematic_main_onclick");
+    item_1.click();
+    //activity .ui.NewsItemPagerActivity
   }
 
   @Test
   public void testAddBookmark() {
-
+    //class android.view.View
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.abc.abcnews:id/cinematic_main_onclick");
+    item_1.click();
+    //class android.widget.TextView
+    //activity .ui.NewsItemPagerActivity
+    AndroidElement save_bookmark = (AndroidElement) driver.findElementById("com.abc.abcnews:id/menu_save_button");
+    save_bookmark.click();
+    //activity .ui.NewsItemPagerActivity
   }
 
   @Test
   public void testRemoveBookmark() {
-
+    //class android.widget.ImageButton
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+    menu.click();
+    //class android.view.ViewGroup
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu_bookmark = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout[2]/android.widget.ListView/android.view.ViewGroup[2]");
+    menu_bookmark.click();
+    //class android.view.ViewGroup
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement item_1 = (AndroidElement) driver.findElementById("com.abc.abcnews:id/headline_list_item_parent_container");
+    item_1.click();
+    //class android.widget.TextView
+    //activity .ui.NewsItemPagerActivity
+    AndroidElement remove_bookmark = (AndroidElement) driver.findElementById("com.abc.abcnews:id/menu_save_button");
+    remove_bookmark.click();
+    //activity .ui.NewsItemPagerActivity
   }
 
-  @Test
-  public void testAccount() {
-
-  }
+  // @Test
+  // public void testAccount() {
+  //   //functionality not present
+  // }
 
   @Test
   public void testTextSize() {
-
+    //class android.widget.ImageButton
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+    menu.click();
+    //class android.widget.RelativeLayout
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout[2]/android.widget.ListView/android.widget.RelativeLayout");
+    menu_settings.click();
+    //class android.widget.LinearLayout
+    //activity .ui.SettingsActivity
+    AndroidElement menu_textsize = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]");
+    menu_textsize.click();
+    //class android.widget.CheckedTextView
+    //activity .ui.SettingsActivity
+    AndroidElement change_textsize = (AndroidElement) driver.findElementById("android:id/text1");
+    change_textsize.click();
+    //activity .ui.SettingsActivity
   }
 
-  @Test
-  public void testAbout() {
-
-  }
+  // @Test
+  // public void testAbout() {
+  //   //functionality not present
+  // }
 
   @Test
   public void testCategory() {
-
+    //class android.widget.ImageButton
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+    menu.click();
+    //class android.view.ViewGroup
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement cat_1 = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout[2]/android.widget.ListView/android.view.ViewGroup[1]");
+    cat_1.click();
+    //activity .ui.navigation.MainNavigationActivity
   }
 
   @Test
   public void testContact() {
-
+    //class android.widget.ImageButton
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+    menu.click();
+    //class android.widget.RelativeLayout
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout[2]/android.widget.ListView/android.widget.RelativeLayout");
+    menu_settings.click();
+    //class android.widget.TextView
+    //activity .ui.SettingsActivity
+    AndroidElement contact = (AndroidElement) driver.findElementById("com.abc.abcnews:id/setting_feedback");
+    contact.click();
+    //activity .ui.WebViewActivity
   }
 
-  @Test
-  public void testHelp() {
-
-  }
+  // @Test
+  // public void testHelp() {
+  //   //functionality not present
+  // }
 
   @Test
   public void testMenu() {
-
+    //class android.widget.ImageButton
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+    menu.click();
+    //activity .ui.navigation.MainNavigationActivity
   }
 
   @Test
   public void testTerms() {
-
+    //class android.widget.ImageButton
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu = (AndroidElement) driver.findElementByXPath("//android.widget.ImageButton[@content-desc=\"Drawer Opened\"]");
+    menu.click();
+    //class android.widget.RelativeLayout
+    //activity .ui.navigation.MainNavigationActivity
+    AndroidElement menu_settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.LinearLayout[2]/android.widget.ListView/android.widget.RelativeLayout");
+    menu_settings.click();
+    //class android.widget.TextView
+    //activity .ui.SettingsActivity
+    AndroidElement legal = (AndroidElement) driver.findElementById("com.abc.abcnews:id/setting_terms");
+    legal.click();
+    //activity .ui.WebViewActivity
   }
 }
