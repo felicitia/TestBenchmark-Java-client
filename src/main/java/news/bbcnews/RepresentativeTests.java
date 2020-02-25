8a308172d6b945c4ac9e4e1560a3cc46f750dd96
felicitia <felicitia2010@gmail.com>
2020-02-24 21:49:13 -0800

    add bbc UI dumps and tests

--- src/main/java/news/bbcnews/RepresentativeTests.java
+++ src/main/java/news/bbcnews/RepresentativeTests.java
@@ -41,73 +41,94 @@ public class RepresentativeTests {
       driver.resetApp();
   }
 
-  @Test
-  public void testSignIn() {
-
-  }
-
-  @Test
-  public void testSignUp() {
-
-  }
 
   @Test
   public void testSearch() {
+    AndroidElement search = (AndroidElement) driver.findElementById("bbc.mobile.news.ww:id/action_search");
+    search.click();
+    AndroidElement search_bar = (AndroidElement) driver.findElementById("bbc.mobile.news.ww:id/search");
+    search_bar.sendKeys("aa\n");
 
   }
 
   @Test
   public void testDetail() {
-
+    //search
+    AndroidElement search = (AndroidElement) driver.findElementById("bbc.mobile.news.ww:id/action_search");
+    search.click();
+    AndroidElement search_bar = (AndroidElement) driver.findElementById("bbc.mobile.news.ww:id/search");
+    search_bar.sendKeys("aa\n");
+    AndroidElement detail = (AndroidElement) driver.findElementById("bbc.mobile.news.ww:id/content_card_image");
+    detail.click();
   }
 
-  @Test
-  public void testAddBookmark() {
-
-  }
-
-  @Test
-  public void testRemoveBookmark() {
-
-  }
-
-  @Test
-  public void testAccount() {
-
-  }
 
   @Test
   public void testTextSize() {
-
+    AndroidElement more_option = (AndroidElement) driver.findElementByXPath("/android.widget.ImageView[@content-desc=\"More options\"]");
+    more_option.click();
+    AndroidElement settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout");
+    settings.click();
+    AndroidElement text_size = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.RelativeLayout");
+    text_size.click();
   }
 
   @Test
   public void testAbout() {
-
+    AndroidElement more_option = (AndroidElement) driver.findElementByXPath("/android.widget.ImageView[@content-desc=\"More options\"]");
+    more_option.click();
+    AndroidElement settings = (AndroidElement) driver.findElementById("bbc.mobile.news.ww:id/content");
+    settings.click();
+    AndroidElement about = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[8]/android.widget.RelativeLayout");
+    about.click();
   }
 
   @Test
   public void testCategory() {
+    AndroidElement category = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.LinearLayout/android.view.ViewGroup/android.widget.ImageButton");
+    category.click();
+    AndroidElement cat_1 = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[2]");
+    cat_1.click();
 
   }
 
   @Test
   public void testContact() {
+    AndroidElement more_option = (AndroidElement) driver.findElementByXPath("/android.widget.ImageView[@content-desc=\"More options\"]");
+    more_option.click();
+    AndroidElement settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout");
+    settings.click();
+    AndroidElement contact = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[3]/android.widget.LinearLayout");
+    contact.click();
 
   }
 
   @Test
   public void testHelp() {
+    AndroidElement more_option = (AndroidElement) driver.findElementByXPath("/android.widget.ImageView[@content-desc=\"More options\"]");
+    more_option.click();
+    AndroidElement settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout");
+    settings.click();
+    AndroidElement help = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout");
+    help.click();
 
   }
 
   @Test
   public void testMenu() {
+    AndroidElement more_option = (AndroidElement) driver.findElementByXPath("/android.widget.ImageView[@content-desc=\"More options\"]");
+    more_option.click();
 
   }
 
   @Test
   public void testTerms() {
+    AndroidElement more_option = (AndroidElement) driver.findElementByXPath("/android.widget.ImageView[@content-desc=\"More options\"]");
+    more_option.click();
+    AndroidElement settings = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[1]/android.widget.LinearLayout");
+    settings.click();
+    AndroidElement legal = (AndroidElement) driver.findElementByXPath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.ListView/android.widget.LinearLayout[5]");
+    legal.click();
 
   }
 }
